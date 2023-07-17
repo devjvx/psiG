cd8301c6e8a6e552c6e24d52e68f698218ee42fa
Janzent Vapor <43449794+devjvx@users.noreply.github.com>
2023-07-17 16:20:40 +0800

    Cleaned up the output

--- src/psiG.ps1
+++ src/psiG.ps1
@@ -1,6 +1,6 @@
 # psiG Script
-# Version 1.0
-# Brought to you by Janzent Vapor :>>
+# Version 1.1
+# Computer Information Retrieval Script :>
 # https://github.com/devjvx/psiG
 
 ############################
@@ -27,11 +27,11 @@ $ScriptDateTime = Get-Date -Format "yyyy/MM/dd HH:mm"
 $ScriptDateTimeNumbers = Get-Date -Format "yyyy-MM-dd_HH-mm"
 
 Write-Output "`n"
-Write-Output " [psiG] Running psiG Script v1.0"
+Write-Output " [psiG] Running psiG Script v1.1"
 
 Start-Sleep 3
 
-Set-ConsoleColor 'black' 'white' 
+Set-ConsoleColor 'black' 'white'
 
 Write-Output "      ___           ___                       ___     "
 Write-Output "     /\  \         /\  \          ___        /\  \    "
@@ -53,7 +53,7 @@ Write-Output " Hello there!"
 Start-Sleep 2
 
 Write-Output "`n"
-Write-Output " This script is brought to you by Janzent Vapor :>>"
+Write-Output " This script is brought to you by Janzent Vapor :>"
 
 Start-Sleep 3
 
@@ -65,6 +65,19 @@ Start-Sleep 3
 
 Write-Output "`n"
 Write-Output " Great! Nice to meet you, $UserName."
+
+Start-Sleep 3
+
+# Write-Output "`n"
+# Write-Output " Quick question, do you prefer to export this as a text file or will you submitting this?"
+# Write-Output " Option 1: Text File"
+# Write-Output " Option 2: CSV File (for submission)"
+
+# $FileExportType = Read-Host " Enter your choice (1-2)"
+
+
+
+Write-Output "`n"
 Write-Output " [psiG] I'll be running the script for you now."
 
 Start-Sleep 5
@@ -79,9 +92,9 @@ Write-Output "      ____  _____(_) ____/ "
 Write-Output "     / __ \/ ___/ / / __   "
 Write-Output "    / /_/ (__  ) / /_/ /   "
 Write-Output "   / .___/____/_/\____/    "
-Write-Output "  /_/                        by Janzent V. :>>"
+Write-Output "  /_/                      "
 Write-Output " =========================="
-Write-Output " v1.0 "
+Write-Output " v1.1 "
 
 Start-Sleep 5
 
@@ -100,7 +113,7 @@ switch -Wildcard ($ComputerOS) {
     "10.0.*" {$OS = "Windows 10"; break}
     default {$OS = "Unknown Operating System"; break}
 }
-Write-Output " [psiG] Done!"
+Write-Output " [psiG] Done retrieving Operating System information."
 
 Start-Sleep 2
 
@@ -108,19 +121,25 @@ Start-Sleep 2
 Write-Output "`n"
 Write-Output " [psiG] Retrieving Motherboard Data..."
 $ComputerMobo = Get-WmiObject -Class Win32_BaseBoard | 
-        Format-Table Manufacturer, Model, Product, SerialNumber, Version
+        Format-List Manufacturer, Model, Product, SerialNumber, Version
+Write-Output " [psiG] Done retrieving motherboard information."
 
-$ComputerBIOS = Get-CimInstance -ClassName Win32_BIOS
-Write-Output " [psiG] Done!"
+Start-Sleep 2
+
+# BIOS Retrieval
+Write-Output "`n"
+Write-Output " [psiG] Retrieving BIOS Data..."
+$ComputerBIOS = Get-CimInstance -ClassName Win32_BIOS |
+        Format-List SMBIOSBIOSVersion, Manufacturer, SerialNumber, Version
+Write-Output " [psiG] Done retrieving BIOS info."
 
 Start-Sleep 2
 
 # Processor Retrieval       
 Write-Output "`n"
 Write-Output " [psiG] Retrieving Processor Data..." 
-$ComputerCPU = Get-WmiObject win32_processor |
-        Select-Object DeviceID,Name | Format-Table -AutoSize
-Write-Output " [psiG] Done!"
+$ComputerCPU = Get-WmiObject win32_processor | Format-List
+Write-Output " [psiG] Done retrieving processor information."
 
 Start-Sleep 2
         
@@ -128,13 +147,14 @@ Start-Sleep 2
 Write-Output "`n"
 Write-Output " [psiG] Retrieving Memory Data..."
 $ComputerRAM = Get-WmiObject Win32_PhysicalMemory |
-        Select-Object DeviceLocator,Manufacturer,PartNumber,@{n="Capacity";e={[math]::Round($_.Capacity/1GB,2)}},Speed | Format-Table -AutoSize
+        Select-Object DeviceLocator,Manufacturer,PartNumber,@{n="Capacity";e={[math]::Round($_.Capacity/1GB,2)}},Speed | Format-List
+
 
 # RAM Capacity Retrieval
-Write-Output " [psiG] Retrieving Memory Capacity Data..."
-$ComputerRam_Total = Get-WmiObject Win32_PhysicalMemoryArray |
-        Select-Object MemoryDevices,MaxCapacity | Format-Table -AutoSize
-Write-Output " [psiG] Done!"
+# Write-Output " [psiG] Retrieving Memory Capacity Data..."
+# $ComputerRam_Total = Get-WmiObject Win32_PhysicalMemoryArray |
+#         Select-Object MemoryDevices,MaxCapacity | Format-List
+# Write-Output " [psiG] Done getting the memory info."
 
 Start-Sleep 2
 
@@ -143,18 +163,20 @@ Write-Output "`n"
 Write-Output " [psiG] Retrieving Storage Disks Data..."
 $ComputerDisks = Get-WmiObject -Class Win32_LogicalDisk -Filter "DriveType=3" |
         Select-Object DeviceID,VolumeName,@{n="Size";e={[math]::Round($_.Size/1GB,2)}},@{n="FreeSpace";e={[math]::Round($_.FreeSpace/1GB,2)}} | Format-Table -AutoSize
-Write-Output " [psiG] Done!"
+Write-Output " [psiG] Done retrieving the storage data."
 
 Start-Sleep 2
+
 # Graphics Retrieval
 Write-Output "`n"
 Write-Output " [psiG] Retrieving Graphics Data..."
 try {
     Start-Sleep 2
     Write-Output " [psiG] Attempting to retrieve Graphics Data."
-    $ComputerGPU = Get-WmiObject win32_VideoController
+    $ComputerGPU = Get-WmiObject win32_VideoController | 
+        Select-Object SystemName, Name, CurrentHorizontalResolution, CurrentVerticalResolution, MaxRefreshRate, MinRefreshRate | 
+        Format-List
     Write-Output " [psiG] Success! Graphics Data retrieval successful."
-    Write-Output " [psiG] Done!"
 } catch {
     Write-Output " [psiG] Error. Failed to retrieve Graphics Data."
 }
@@ -164,29 +186,30 @@ Start-Sleep 5
 Write-Output "`n"
 Write-Output " [psiG] Consolidating all data gathered..."
 try {
-    "[psiG Script]`nBrought to you by Janzent Vapor :>> (https://github.com/devjvx)" | Out-File -FilePath psiG-Output-$Username-$ScriptDateTimeNumbers.txt
+    "[psiG Script]`nComputer Information Retrievel Script v1.1 (https://github.com/devjvx)" | Out-File -FilePath psiG-Output-$Username-$ScriptDateTimeNumbers.txt
     "`nComputer is used by: $UserName" | Out-File -FilePath psiG-Output-$Username-$ScriptDateTimeNumbers.txt -Append
-    "`nScript Date/Time: $ScriptDateTime" | Out-File -FilePath psiG-Output-$Username-$ScriptDateTimeNumbers.txt -Append
+    "`nScript ran at Date/Time: $ScriptDateTime `n" | Out-File -FilePath psiG-Output-$Username-$ScriptDateTimeNumbers.txt -Append
     "======================== OPERATING SYSTEM ======================== " | Out-File -FilePath psiG-Output-$Username-$ScriptDateTimeNumbers.txt -Append
-    "Operating System: $OS" | Out-File -FilePath psiG-Output-$Username-$ScriptDateTimeNumbers.txt -Append
-    $ComputerOSBuild  | Out-File -FilePath psiG-Output-$Username-$ScriptDateTimeNumbers.txt -Append
+    "`nOperating System: $OS" | Out-File -FilePath psiG-Output-$Username-$ScriptDateTimeNumbers.txt -Append
+    $ComputerOSBuild | Out-File -FilePath psiG-Output-$Username-$ScriptDateTimeNumbers.txt -Append
     "======================== MOTHERBOARD ======================== " | Out-File -FilePath psiG-Output-$Username-$ScriptDateTimeNumbers.txt -Append
     $ComputerMobo | Out-File -FilePath psiG-Output-$Username-$ScriptDateTimeNumbers.txt -Append
+    "======================== BIOS ======================== " | Out-File -FilePath psiG-Output-$Username-$ScriptDateTimeNumbers.txt -Append
     $ComputerBIOS | Out-File -FilePath psiG-Output-$Username-$ScriptDateTimeNumbers.txt -Append
     "======================== CPU ======================== " | Out-File -FilePath psiG-Output-$Username-$ScriptDateTimeNumbers.txt -Append
     $ComputerCPU  | Out-File -FilePath psiG-Output-$Username-$ScriptDateTimeNumbers.txt -Append
     "======================== MEMORY ======================== " | Out-File -FilePath psiG-Output-$Username-$ScriptDateTimeNumbers.txt -Append
-    $ComputerRAM_Total  | Out-File -FilePath psiG-Output-$Username-$ScriptDateTimeNumbers.txt -Append
     $ComputerRAM  | Out-File -FilePath psiG-Output-$Username-$ScriptDateTimeNumbers.txt -Append
+    # $ComputerRAM_Total  | Out-File -FilePath psiG-Output-$Username-$ScriptDateTimeNumbers.txt -Append
     "======================== STORAGE ======================== " | Out-File -FilePath psiG-Output-$Username-$ScriptDateTimeNumbers.txt -Append
     $ComputerDisks  | Out-File -FilePath psiG-Output-$Username-$ScriptDateTimeNumbers.txt -Append
     "======================== GRAPHICS ======================== " | Out-File -FilePath psiG-Output-$Username-$ScriptDateTimeNumbers.txt -Append
     $ComputerGPU  | Out-File -FilePath psiG-Output-$Username-$ScriptDateTimeNumbers.txt -Append
     "-------------------------------- END OF FILE --------------------------------" | Out-File -FilePath psiG-Output-$Username-$ScriptDateTimeNumbers.txt -Append
 
-    Write-Output " [psiG] Done!"
+    Write-Output " [psiG] Done exporting to a file! See the location of the script."
 } catch {
-    Write-Output " [psiG] Error. Unable to export consolidated data. Check permissions."
+    Write-Output " [psiG] Error. Unable to export consolidated data. Check file permissions."
 }
 
 Start-Sleep 4
